f0bc409f8000f4a523e8d782c9e3db4e6aaac99a
rocioortiz14 <angelelcielo@gmail.com>
2023-05-15 17:30:04 -0600

    "Hello Word y Variables"

---
+++ 00_hello.py
@@ -0,0 +1,15 @@
+# Esto es un comentario
+# Nuestro hola mundo en python 
+
+"""
+Comentario en varias lineas
+"""
+print("Hola Python") # se puede utilizar comilla doble o simple
+print('Hola Python')
+
+# consulta el tipo de dato 
+print(type ("Soy un dato str")) # imprime el tipo de dato 
+print(type (5))
+print(type (2.5))
+print(type (1 + 2j))
+print(type (True))
